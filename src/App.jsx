5d9e51710b7395e7ba79b49fbbfdb0406bca54a4
elodaimond1-hub <elodaimond1@gmail.com>
2025-08-24 22:15:08 +0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React, { useState, useEffect, useRef } from 'react';
 import identity from './assets/identity.jpg';
 import buttons from './assets/buttons.jpg';
+import close from './assets/close.png';
+import QR from './assets/QR.png';
 import './App.css';
 
 const App = () => {
   const [selectedImage, setSelectedImage] = useState('');
+  const [toastVisible, setToastVisible] = useState(false);
+  const [randomNumber, setRandomNumber] = useState(null);
   const fileInputRef = useRef(null);
 
   useEffect(() => {
@@ -32,6 +36,16 @@ const App = () => {
       fileInputRef.current.click();
     }
   };
+
+  const showId = () => {
+    const num = Math.floor(100000 + Math.random() * 900000);
+    setRandomNumber(num);
+    setToastVisible(true);
+  }
+
+  const closeId = () => {
+    setToastVisible(false);
+  }
 
   return (
     <div className="app">
@@ -62,10 +76,21 @@ const App = () => {
         </div>
 
         <div className="separator-line"></div>
-
         <div className="bottom_btn">
-          <img src={buttons} alt="" className="down" />
-          <button className="btn"></button>
+          <img src={buttons} alt="" className="down"/>
+          <button className="btn-share"></button>1
+          <button className="btn-show" onClick={showId}></button>
+          <div className={`overlay ${toastVisible ? 'show' : ''}`}></div>
+          <div className={`toast-bottom ${toastVisible ? 'show' : ''}`}>
+            <button className="toast-close-btn" onClick={closeId}>
+              <img className="toast-close-btn-img" src={close}></img>
+            </button>
+            <h3 className="toast-title">Удостоверение личности</h3>
+            <span className="toast-padding">Покажите QR-код сотруднику</span>
+            <img className="qr-img" src={QR}></img>
+            <span>или скажите код</span>
+            <h2>{randomNumber}</h2>
+          </div>
         </div>
       </div>
     </div>
